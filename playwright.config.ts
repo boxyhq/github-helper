218c516e18d08fee4b65d0cd5faf58d2653cf161
Deepak Prabhakara <deepak@boxyhq.com>
2023-12-12 00:04:39 +0000

    switched to port 5500

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,12 +22,12 @@ const config: PlaywrightTestConfig = {
   ],
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:5000',
+    url: 'http://localhost:5500',
   },
   use: {
     headless: true,
     ignoreHTTPSErrors: true,
-    baseURL: 'http://localhost:5000',
+    baseURL: 'http://localhost:5500',
     video: 'off',
   },
 };
